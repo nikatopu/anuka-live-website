86cfa1014b08cb09eba4915280b2901da04c6059
nikatopu <nikatopu@gmail.com>
2025-01-31 13:56:36 +0400

    Changed the Temporary Look

---
+++ src/Temporary/Temporary.jsx
@@ -0,0 +1,14 @@
+import "./temporary.css";
+function Temporary() {
+    return (
+        <div className="App-Temporary">
+            <a href="https://www.linkedin.com/in/akipsidze/" target="_blank" rel="noopener">
+                <img src="/ანუკა.jpg" alt="Anuka" className="round-image"/>
+            </a>
+            <h1>Stalking <span>Anuka</span>, are we?</h1>
+            <h2>It's okay, we get it, come back later for the website</h2>
+        </div>
+    )
+}
+
+export default Temporary;
