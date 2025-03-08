9ffd2abb10b66b596c97e5e8fa5d05cdce3628f3
nikatopu <nikatopu@gmail.com>
2025-03-08 17:01:47 +0400

    Fix/ removed the news text on news

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -6,10 +6,6 @@ import './News.scss'
 function News() {
   return (
     <div className='section-news' id='section-news'>
-      <div className='section-news-header'>
-        <h1>News</h1>
-        <p>I am Anuka Kipshidze (ანუკა ყიფშიძე), a musician, producer, sound designer, and multi-instrumentalist from Tbilisi, Georgia.</p>
-      </div>
       <AllNews blogs={newsBlogs.blogs}/>
     </div>
   )
