e1cf5e7555705256b4333e7b3c75face309555c8
nikatopu <nikatopu@gmail.com>
2025-03-09 22:40:13 +0400

    Feat/ Added the contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import ContactForm from '../../content/pages/home/contact/form/ContactForm'
+import './Contact.scss'
 
 function Contact() {
   return (
-    <div></div>
+    <div className="section-contact-page">
+      <div className="section-contact-page-left">
+        <div className="section-contact-page-left-top">
+          <h2>
+            Let's Talk
+          </h2>
+          <p>
+            Looking to start a project or just need to collaborate? Feel Free to contact me.  
+          </p>
+          <a href='mailto:contact@anuka.live'>contact@anuka.live</a>
+        </div>
+        <div className="section-contact-page-left-bottom">
+        <ContactForm />
+        </div>
+      </div>
+      <div className="section-contact-page-right">
+        <img src="/img/pages/contact/background.webp" alt="background decoration"/>
+      </div>
+    </div>
   )
 }
 
